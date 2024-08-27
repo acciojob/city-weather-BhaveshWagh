e442ab865e86a596bcca52ee55a510b3c53b28d2
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-08-27 12:34:14 +0530

    done 1.1

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,5 @@
+import "regenerator-runtime/runtime";
+
 import React, { useState, useEffect } from "react";
 import "./../styles/App.css";
 
@@ -9,31 +11,25 @@ const App = () => {
 
   useEffect(() => {
     const fetchApi = async () => {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=2d19741fec5e1ebbce4e4b02007b3734`;
-      try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          setError("City not found");
-          throw new Error("City not found");
-        }
-        setError("");
-        const data = await response.json();
-        setWeather(data);
-      } catch (error) {
-        setWeather({ error: error.message });
-      }
+      const url =
+        "https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=2d19741fec5e1ebbce4e4b02007b3734";
+      fetch(url)
+        .then((response) => {
+          if (!response.ok) {
+            setError("City not found");
+            throw new Error("City not found");
+          }
+          setError("");
+          return response.json();
+        })
+        .then((data) => setWeather(data))
+        .catch((error) => setWeather({ error: error.message }));
     };
-
-    if (city) {
-      fetchApi();
-    }
+    fetchApi();
   }, [city]);
 
   function display() {
-    if (search) {
-      setCity(search);
-      setSearch(""); // Clear the search input after a valid city is entered
-    }
+    setCity(search);
   }
 
   return (
@@ -42,7 +38,6 @@ const App = () => {
         <input
           type="text"
           placeholder="Enter a city"
-          value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
         <button onClick={display}>Search</button>
@@ -52,11 +47,11 @@ const App = () => {
         {weather.main && (
           <>
             <h1>{city}</h1>
-            <h2>{weather.main.temp}°C</h2>
+            <h2>{weather.main.temp}°Cel</h2>
             <h3>{weather.weather[0].description}</h3>
             <img
               src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
-              alt="weather icon"
+              alt="cloud image"
             />
           </>
         )}
